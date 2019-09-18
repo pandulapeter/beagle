39becd6bb0e235f61e4ed817472d46f06e4b4d92
Péter Pandula <peter@halcyonmobile.com>
2019-09-18 18:25:53 +0300

    Fix incorrect noop implementation for the Interceptor.

--- debug-menu-noop/src/main/java/com/pandulapeter/debugMenu/DebugMenuInterceptor.kt
+++ debug-menu-noop/src/main/java/com/pandulapeter/debugMenu/DebugMenuInterceptor.kt
@@ -4,7 +4,7 @@ import com.pandulapeter.debugMenuCore.DebugMenuInterceptor
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class DebugMenuInterceptor : DebugMenuInterceptor {
+object DebugMenuInterceptor : DebugMenuInterceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response = chain.proceed(chain.request())
 }
