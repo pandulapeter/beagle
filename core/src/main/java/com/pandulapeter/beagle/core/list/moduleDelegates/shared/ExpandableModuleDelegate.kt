b12b2f1d890ca129698b63008d3e3619093e1b22
Péter Pandula <peter@halcyonmobile.com>
2020-06-06 19:19:42 +0300

    Create the ExpandableModuleDelegate class to reduce code duplication.

---
+++ core/src/main/java/com/pandulapeter/beagle/core/list/moduleDelegates/shared/ExpandableModuleDelegate.kt
@@ -0,0 +1,38 @@
+package com.pandulapeter.beagle.core.list.moduleDelegates.shared
+
+import com.pandulapeter.beagle.BeagleCore
+import com.pandulapeter.beagle.common.contracts.module.Cell
+import com.pandulapeter.beagle.common.contracts.module.ExpandableModule
+import com.pandulapeter.beagle.common.contracts.module.Module
+import com.pandulapeter.beagle.core.list.cells.ExpandableHeaderCell
+
+internal abstract class ExpandableModuleDelegate<M : ExpandableModule<M>> : Module.Delegate<M>() {
+
+    override fun createCells(module: M): List<Cell<*>> = mutableListOf<Cell<*>>().apply {
+        addHeader(module)
+        if (module.isExpanded) {
+            addItems(module)
+        }
+    }
+
+    private fun MutableList<Cell<*>>.addHeader(module: M) = add(
+        ExpandableHeaderCell(
+            id = module.id,
+            text = module.title,
+            color = module.color,
+            isExpanded = module.isExpanded,
+            onItemSelected = {
+                module.isExpanded = !module.isExpanded
+                BeagleCore.implementation.refreshCells()
+            }
+        )
+    )
+
+    protected abstract fun MutableList<Cell<*>>.addItems(module: M)
+
+    private var ExpandableModule<M>.isExpanded: Boolean
+        get() = BeagleCore.implementation.memoryStorageManager.booleans[id] ?: isExpandedInitially
+        set(value) {
+            BeagleCore.implementation.memoryStorageManager.booleans[id] = value
+        }
+}
