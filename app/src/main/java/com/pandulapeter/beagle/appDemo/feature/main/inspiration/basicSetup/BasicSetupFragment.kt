1fecacbbc07198fb452133f31e7747bc47aaf8b1
Péter Pandula <peter@halcyonmobile.com>
2020-06-11 11:18:47 +0300

    Add mock list to the Basic Setup detail screen.

--- app/src/main/java/com/pandulapeter/beagle/appDemo/feature/main/inspiration/basicSetup/BasicSetupFragment.kt
+++ app/src/main/java/com/pandulapeter/beagle/appDemo/feature/main/inspiration/basicSetup/BasicSetupFragment.kt
@@ -1,10 +1,16 @@
 package com.pandulapeter.beagle.appDemo.feature.main.inspiration.basicSetup
 
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.transition.MaterialSharedAxis
+import com.pandulapeter.beagle.Beagle
 import com.pandulapeter.beagle.appDemo.R
 import com.pandulapeter.beagle.appDemo.databinding.FragmentBasicSetupBinding
 import com.pandulapeter.beagle.appDemo.feature.shared.BaseViewModelFragment
+import com.pandulapeter.beagle.appDemo.feature.shared.list.BaseAdapter
+import com.pandulapeter.beagle.appDemo.feature.shared.list.ListItem
+import com.pandulapeter.beagle.modules.TextModule
 import org.koin.android.ext.android.inject
 
 class BasicSetupFragment : BaseViewModelFragment<FragmentBasicSetupBinding, BasicSetupViewModel>(R.layout.fragment_basic_setup, false, R.string.case_study_basic_setup_title) {
@@ -18,6 +24,26 @@ class BasicSetupFragment : BaseViewModelFragment<FragmentBasicSetupBinding, Basi
         reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
         exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        refreshBeagleModules()
+    }
+
+    private fun setupRecyclerView() {
+        val recyclerAdapter = object : BaseAdapter<ListItem>() {}
+        binding.recyclerView.run {
+            adapter = recyclerAdapter
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+        }
+        viewModel.items.observeForever(recyclerAdapter::submitList)
+    }
+
+    private fun refreshBeagleModules() = Beagle.setModules(
+        TextModule(text = "Work in progress")
+    )
 
     companion object {
         fun newInstance() = BasicSetupFragment()
--- app/src/main/java/com/pandulapeter/beagle/appDemo/feature/main/inspiration/basicSetup/BasicSetupViewModel.kt
+++ app/src/main/java/com/pandulapeter/beagle/appDemo/feature/main/inspiration/basicSetup/BasicSetupViewModel.kt
@@ -1,5 +1,24 @@
 package com.pandulapeter.beagle.appDemo.feature.main.inspiration.basicSetup
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.pandulapeter.beagle.appDemo.R
+import com.pandulapeter.beagle.appDemo.feature.shared.list.ListItem
+import com.pandulapeter.beagle.appDemo.feature.shared.list.TextViewHolder
 
-class BasicSetupViewModel : ViewModel()
+class BasicSetupViewModel : ViewModel() {
+
+    private val _items = MutableLiveData<List<ListItem>>()
+    val items: LiveData<List<ListItem>> = _items
+
+    init {
+        _items.value = listOf(
+            TextViewHolder.UiModel(R.string.inspiration_beagle_text_1),
+            TextViewHolder.UiModel(R.string.inspiration_beagle_text_1),
+            TextViewHolder.UiModel(R.string.inspiration_beagle_text_1),
+            TextViewHolder.UiModel(R.string.inspiration_beagle_text_1),
+            TextViewHolder.UiModel(R.string.inspiration_beagle_text_1)
+        )
+    }
+}
