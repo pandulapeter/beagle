69a950c4276599a6891f8e9a3a84fe9e0f5937c1
Péter Pandula <peter@halcyonmobile.com>
2020-12-06 15:04:39 +0200

    Finalize the CrashLogging example.

--- app/src/main/java/com/pandulapeter/beagle/appDemo/feature/main/examples/crashLogging/CrashLoggingFragment.kt
+++ app/src/main/java/com/pandulapeter/beagle/appDemo/feature/main/examples/crashLogging/CrashLoggingFragment.kt
@@ -5,7 +5,9 @@ import com.pandulapeter.beagle.appDemo.R
 import com.pandulapeter.beagle.appDemo.feature.main.examples.ExamplesDetailFragment
 import com.pandulapeter.beagle.appDemo.feature.shared.list.BaseAdapter
 import com.pandulapeter.beagle.appDemo.feature.shared.list.ListItem
+import com.pandulapeter.beagle.appDemo.utils.createTextModule
 import com.pandulapeter.beagle.common.contracts.module.Module
+import com.pandulapeter.beagle.modules.BugReportButtonModule
 import com.pandulapeter.beagle.modules.ForceCrashButtonModule
 import com.pandulapeter.beagle.modules.TextModule
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -19,8 +21,10 @@ class CrashLoggingFragment : ExamplesDetailFragment<CrashLoggingViewModel, ListI
     ) {}
 
     override fun getBeagleModules(): List<Module<*>> = listOf(
-        TextModule("Coming soon"),
-        ForceCrashButtonModule(type = TextModule.Type.BUTTON)
+        createTextModule(R.string.case_study_crash_logging_hint_1),
+        ForceCrashButtonModule(type = TextModule.Type.BUTTON),
+        createTextModule(R.string.case_study_crash_logging_hint_2),
+        BugReportButtonModule(type = TextModule.Type.BUTTON)
     )
 
     companion object {
--- app/src/main/java/com/pandulapeter/beagle/appDemo/feature/main/examples/crashLogging/CrashLoggingViewModel.kt
+++ app/src/main/java/com/pandulapeter/beagle/appDemo/feature/main/examples/crashLogging/CrashLoggingViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.pandulapeter.beagle.appDemo.R
 import com.pandulapeter.beagle.appDemo.feature.shared.ListViewModel
+import com.pandulapeter.beagle.appDemo.feature.shared.list.CodeSnippetViewHolder
 import com.pandulapeter.beagle.appDemo.feature.shared.list.ListItem
 import com.pandulapeter.beagle.appDemo.feature.shared.list.TextViewHolder
 
@@ -11,7 +12,30 @@ class CrashLoggingViewModel : ListViewModel<ListItem>() {
 
     override val items: LiveData<List<ListItem>> = MutableLiveData(
         listOf(
-            TextViewHolder.UiModel(R.string.case_study_crash_logging_description)
+            TextViewHolder.UiModel(R.string.case_study_crash_logging_text_1),
+            CodeSnippetViewHolder.UiModel(
+                "dependencies {\n" +
+                        "    …\n" +
+                        "    debugImplementation \"com.github.pandulapeter.beagle:log-crash:\$beagleVersion\"\n" +
+                        "    releaseImplementation \"com.github.pandulapeter.beagle:log-crash-noop:\$beagleVersion\"\n" +
+                        "}"
+            ),
+            TextViewHolder.UiModel(R.string.case_study_crash_logging_text_2),
+            CodeSnippetViewHolder.UiModel(
+                "Beagle.initialize(\n" +
+                        "    …\n" +
+                        "    behavior = Behavior(\n" +
+                        "        …\n" +
+                        "        bugReportingBehavior = Behavior.BugReportingBehavior(\n" +
+                        "            crashLoggers = listOf(BeagleCrashLogger),\n" +
+                        "            …\n" +
+                        "        )\n" +
+                        "    )\n" +
+                        ")"
+            ),
+            TextViewHolder.UiModel(R.string.case_study_crash_logging_text_3),
+            CodeSnippetViewHolder.UiModel("FirebaseApp.initializeApp(this)"),
+            TextViewHolder.UiModel(R.string.case_study_crash_logging_text_4),
         )
     )
 }
