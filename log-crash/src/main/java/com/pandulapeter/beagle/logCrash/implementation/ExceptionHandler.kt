c12e9c7e60f202a7740e486bfbdc2c15d1326882
Péter Pandula <peter@halcyonmobile.com>
2021-01-04 15:28:49 +0200

    Simplify the ExceptionHandler logic.

--- log-crash/src/main/java/com/pandulapeter/beagle/logCrash/implementation/ExceptionHandler.kt
+++ log-crash/src/main/java/com/pandulapeter/beagle/logCrash/implementation/ExceptionHandler.kt
@@ -1,12 +1,7 @@
 package com.pandulapeter.beagle.logCrash.implementation
 
 import android.app.Application
-import android.content.ComponentName
-import android.content.Context
 import android.content.Intent
-import android.content.ServiceConnection
-import android.os.IBinder
-import android.os.Messenger
 import android.os.Process
 import com.pandulapeter.beagle.BeagleCore
 import com.pandulapeter.beagle.commonBase.currentTimestamp
@@ -19,18 +14,32 @@ import com.pandulapeter.beagle.core.view.bugReport.BugReportActivity
 import kotlin.system.exitProcess
 
 internal class ExceptionHandler private constructor(
-    private val messenger: Messenger
+    private val application: Application
 ) : Thread.UncaughtExceptionHandler {
+
+    private val limit by lazy { BeagleCore.implementation.behavior.bugReportingBehavior.logRestoreLimit }
+    private val logsToRestore
+        get() = BeagleCore.implementation.getLogEntries(null).take(limit)
+    private val networkLogsToRestore
+        get() = BeagleCore.implementation.getNetworkLogEntries().take(limit)
+    private val lifecycleLogsToRestore
+        get() = BeagleCore.implementation.getLifecycleLogEntries(BeagleCore.implementation.behavior.bugReportingBehavior.lifecycleSectionEventTypes).take(limit)
+    private var lastCrashTimestamp = 0L
 
     override fun uncaughtException(thread: Thread, exception: Throwable) {
         try {
-            messenger.send(
-                ExceptionHandlerService.getMessage(
-                    crashLogEntry = exception.toCrashLogEntry(),
-                    logs = logsToRestore,
-                    networkLogs = networkLogsToRestore,
-                    lifecycleLogs = lifecycleLogsToRestore
-                )
+            application.startActivity(
+                BugReportActivity.newIntent(
+                    context = application,
+                    crashLogEntryToShowJson = crashLogEntryAdapter.toJson(exception.toCrashLogEntry()),
+                    restoreModelJson = restoreModelAdapter.toJson(
+                        RestoreModel(
+                            logs = logsToRestore,
+                            networkLogs = networkLogsToRestore,
+                            lifecycleLogs = lifecycleLogsToRestore
+                        )
+                    )
+                ).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             )
             tryToCallDefaultExceptionHandler(thread, exception)
         } catch (_: Exception) {
@@ -39,87 +48,45 @@ internal class ExceptionHandler private constructor(
         }
     }
 
+    private fun Throwable.toCrashLogEntry() = CrashLogEntry(
+        id = randomId,
+        exception = message ?: this::class.java.simpleName,
+        stacktrace = stackTraceToString(),
+        timestamp = currentTimestamp
+    )
+
+    private fun tryToCallDefaultExceptionHandler(thread: Thread, exception: Throwable) {
+        defaultExceptionHandler.let { defaultExceptionHandler ->
+            if (currentTimestamp - lastCrashTimestamp > CRASH_LOOP_LIMIT
+                && defaultExceptionHandler != null
+                && defaultExceptionHandler::class.java.canonicalName?.startsWith("com.android.internal.os") != true
+            ) {
+                lastCrashTimestamp = currentTimestamp
+                defaultExceptionHandler.uncaughtException(thread, exception)
+            }
+        }
+    }
+
+    private fun killProcess() {
+        Process.killProcess(Process.myPid())
+        exitProcess(10)
+    }
+
     companion object {
-        private const val CRASH_LOOP_LIMIT = 2000L
+        private const val CRASH_LOOP_LIMIT = 500L
         private var isInitialized = false
         private var defaultExceptionHandler: Thread.UncaughtExceptionHandler? = null
-        private var lastCrashTimestamp = 0L
-        private val limit by lazy { BeagleCore.implementation.behavior.bugReportingBehavior.logRestoreLimit }
-        private val logsToRestore
-            get() = BeagleCore.implementation.getLogEntries(null).take(limit)
-        private val networkLogsToRestore
-            get() = BeagleCore.implementation.getNetworkLogEntries().take(limit)
-        private val lifecycleLogsToRestore
-            get() = BeagleCore.implementation.getLifecycleLogEntries(BeagleCore.implementation.behavior.bugReportingBehavior.lifecycleSectionEventTypes).take(limit)
 
         fun initialize(application: Application) {
             if (!isInitialized) {
-                isInitialized = true
                 Thread.getDefaultUncaughtExceptionHandler()?.let {
                     if (it !is ExceptionHandler) {
                         defaultExceptionHandler = it
+                        Thread.setDefaultUncaughtExceptionHandler(ExceptionHandler(application))
+                        isInitialized = true
                     }
                 }
-                Thread.setDefaultUncaughtExceptionHandler { thread, exception ->
-                    try {
-                        application.startActivity(
-                            BugReportActivity.newIntent(
-                                context = application,
-                                crashLogEntryToShowJson = crashLogEntryAdapter.toJson(exception.toCrashLogEntry()),
-                                restoreModelJson = restoreModelAdapter.toJson(
-                                    RestoreModel(
-                                        logs = logsToRestore,
-                                        networkLogs = networkLogsToRestore,
-                                        lifecycleLogs = lifecycleLogsToRestore
-                                    )
-                                )
-                            ).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                        )
-                        tryToCallDefaultExceptionHandler(thread, exception)
-                    } catch (_: Exception) {
-                    } finally {
-                        killProcess()
-                    }
-                }
-                application.bindService(
-                    Intent(application, ExceptionHandlerService::class.java),
-                    object : ServiceConnection {
-
-                        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-                            if (service != null) {
-                                Thread.setDefaultUncaughtExceptionHandler(ExceptionHandler(Messenger(service)))
-                            }
-                        }
-
-                        override fun onServiceDisconnected(name: ComponentName?) = Unit
-                    },
-                    Context.BIND_AUTO_CREATE
-                )
             }
-        }
-
-        private fun Throwable.toCrashLogEntry() = CrashLogEntry(
-            id = randomId,
-            exception = message ?: this::class.java.simpleName,
-            stacktrace = stackTraceToString(),
-            timestamp = currentTimestamp
-        )
-
-        private fun tryToCallDefaultExceptionHandler(thread: Thread, exception: Throwable) {
-            defaultExceptionHandler.let { defaultExceptionHandler ->
-                if (currentTimestamp - lastCrashTimestamp > CRASH_LOOP_LIMIT
-                    && defaultExceptionHandler != null
-                    && defaultExceptionHandler::class.java.canonicalName?.startsWith("com.android.internal.os") != true
-                ) {
-                    lastCrashTimestamp = currentTimestamp
-                    defaultExceptionHandler.uncaughtException(thread, exception)
-                }
-            }
-        }
-
-        private fun killProcess() {
-            Process.killProcess(Process.myPid())
-            exitProcess(10)
         }
     }
 }
