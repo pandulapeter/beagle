d78a8782ae0b539d207d77701d65f42b81d89a80
Rahul Sainani <rahul.sainani@gorillasapp.com>
2021-11-21 22:00:43 +0100

    Use WindowCompat to handle system windows over deprecated flags

--- ui-bottom-sheet/src/main/java/com/pandulapeter/beagle/logCrash/implementation/DebugMenuBottomSheet.kt
+++ ui-bottom-sheet/src/main/java/com/pandulapeter/beagle/logCrash/implementation/DebugMenuBottomSheet.kt
@@ -7,6 +7,7 @@ import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -63,7 +64,9 @@ internal class DebugMenuBottomSheet : BottomSheetDialogFragment(), UpdateListene
                 behavior = this
                 addBottomSheetCallback(bottomSheetCallback)
             }
-            dialog?.window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            dialog?.window?.let {
+                WindowCompat.setDecorFitsSystemWindows(it, false)
+            }
         }
     }
 
